ccc476348fc9dff3cf1a88e980562472f21fadb6
Rof1yev <rofiyevdilshod@gmail.com>
2023-09-25 22:59:57 +0500

    added favicon in site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <meta http-equiv="Content-Security-Policy" content="" />
+        <link
+          rel="shortcut icon"
+          href="https://rof1yev-blog.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.cbf9d521.png&w=384&q=75"
+          type="image/x-icon"
+        />
       </Head>
       <StateContext>
         <Layout>
