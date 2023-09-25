fc273e66ca65e60d396d59a4602aeb6d109bdd6a
Rof1yev <rofiyevdilshod@gmail.com>
2023-09-25 22:30:36 +0500

    sanity changed

--- sanity_ecommerce/sanity.cli.js
+++ sanity_ecommerce/sanity.cli.js
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from '@sanity/cli'
 
 export default defineCliConfig({
   api: {
     projectId: 'idx6gsgp',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
